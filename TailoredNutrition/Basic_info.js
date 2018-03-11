c23be4164ebc3dba6e8d35c995bc889a36a3cfb5
Modem68 <lncmpbll4@gmail.com>
2018-03-11 18:33:57 -0400

    Added the fields of text and words for page

--- TailoredNutrition/Basic_info.js
+++ TailoredNutrition/Basic_info.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-    StyleSheet, View, TextInput, Button
+    StyleSheet, View, TextInput, Text, Button
 } from 'react-native';
 
 export default class Basic_Info extends Component<{}> {
@@ -20,7 +20,51 @@ export default class Basic_Info extends Component<{}> {
 		console.log('Basic_info.render');
 		
 		return (
-			
+					<View style = {styles.container}>
+					
+					<Text style = {styles.description}> 
+					Name 
+					</Text>
+					
+					<TextInput style = {styles.inputStyle}
+						value = {this.state.inputValue}
+						onChangeText = {this.TextChange}
+						/>
+						
+					<Text style = {styles.description}> 
+					Age
+					</Text>
+					
+					<TextInput style = {styles.inputStyle}
+						value = {this.state1.inputValue}
+						onChangeText = {this.TextChange}
+						/>
+						
+					
+					</View>
 			)
 	}
 }
+
+const styles = StyleSheet.create({
+                                 description: {
+                                 marginBottom: 20,
+                                 fontSize: 18,
+                                 textAlign: 'center',
+                                 color: '#656565'
+                                 },
+                                 container: {
+                                 padding: 30,
+                                 marginTop: 65,
+                                 alignItems: 'center'
+                                 },
+                                 image: {
+                                 height: 182 / 4,
+                                 width: 995 / 4,
+                                 marginTop: 20,
+                                 },
+								 
+								 inputStyle: {
+                                 width: 375, height: 44, padding: 8, borderWidth: 1, borderRadius: 8, borderColor: '#48BBEC', marginBottom: 4,
+                                 },
+                                 });
