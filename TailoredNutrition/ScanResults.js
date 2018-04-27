8c37901b04c72bc07a27754f28319dddf91f8f32
Andrew Glyadchenko <andrewgly95@gmail.com>
2018-04-27 14:36:25 -0400

    Added display for scanned items

--- TailoredNutrition/ScanResults.js
+++ TailoredNutrition/ScanResults.js
@@ -14,14 +14,32 @@ import { TabNavigator, TabBarBottom, StackNavigator } from 'react-navigation';
 export default class ScanResults extends Component {
   constructor(props) {
     super(props);
-      this.state = {result: "365 Everyday Value Electrolyte Water"};
+      this.state = {result: this.props.navigation.state.params.item};
   }
 
   render() {
     return (
-      <View>
-        <Text>You have scanned {this.state.result}</Text>
+      <View style={container}>
+        <Text style = {title}> {this.state.result.brand_name} {this.state.result.food_name}</Text>
+        <Image
+          style={{width: 250, height: 250}}
+          source={{uri: this.state.result.photo.thumb}}
+        />
+        <Text>Calories: {this.state.result.nf_calories}</Text>
+        <Text>Protein: {this.state.result.nf_protein}</Text>
+        <Text>Fat: {this.state.result.nf_total_fat}</Text>
+        <Text>Carbohydrates: {this.state.result.nf_total_carbohydrate}</Text>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 25,
+  },
+  title: {
+    fontSize: 25,
+  },
+});
